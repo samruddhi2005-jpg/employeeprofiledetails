4b7e8a203c4527934061dd600dda5e89a94db813
samruddhi2005-jpg <samruddhiadlinge2005@gmail.com>
2026-06-06 00:01:42 +0530

    Add files via upload

---
+++ EmployeeProfileTable.js
@@ -0,0 +1,6 @@
+//employee profile table
+document.getElementById("name_cell").textContent=prompt('Enter Employee Name');
+document.getElementById("salary_cell").textContent=prompt('Enter Salary');
+document.getElementById("designation_cell").textContent=prompt('Enter Designation');
+document.getElementById("job_location_cell").textContent=prompt('Enter Job Location');
+document.getElementById("years_of_experience_cell").textContent=prompt('Enter Years Of Experience');
